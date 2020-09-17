35406463a6fa2690d9ad3a941dffa6c71d57e6d3
Xvaria <2349@holbertonschool.com>
2020-09-17 00:51:06 +0000

    Ready

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main - Puts 
+* main - Puts
 * Description: Prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
