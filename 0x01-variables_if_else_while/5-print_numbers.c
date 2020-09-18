c71eec6d20fed775b62bad59979ea3b014334503
Xvaria <2349@holbertonschool.com>
2020-09-18 05:17:15 +0000

    Ready

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - For numbers
+ * Description: Print numbers 0 to 1
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+for (a = 0; a <= 9; a++)
+{
+printf("%i", a);
+}
+return (0);
+}
