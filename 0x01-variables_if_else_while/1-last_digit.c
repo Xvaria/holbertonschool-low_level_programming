12aeff0820c81d4c6eb3019ee81f99d892080112
Xvaria <2349@holbertonschool.com>
2020-09-18 02:16:43 +0000

    Ready

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - If
+ * Description: Print the number in the variable n is positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+a = n % 10;
+printf("Last digit of %i is %i", n, a);
+if (a > 5)
+{
+printf("and is greater than 5\n");
+}
+else if (a == 0)
+{
+printf("and is 0\n");
+}
+else
+{
+printf("and is less than 6 and not 0\n");
+}
+return (0);
+}
