a726d024956ddf912042f9abfc5d511dac0885bf
Xvaria <2349@holbertonschool.com>
2020-09-18 03:14:30 +0000

    Ready

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - If
+ * Description: Print the number in the variable n is positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+int a = 97;
+while (a < 123)
+{
+putchar (a);
+putchar ("\n");
+a++
+}
+return (0);
+}
