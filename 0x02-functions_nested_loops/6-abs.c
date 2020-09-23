0162fe5b01be9f0cf1676339c61f150405e31fd4
Xvaria <2349@holbertonschool.com>
2020-09-23 19:43:34 +0000

    Ready

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * int _abs - print absolute number
+ * @a: integer
+ * Return: 0
+ */
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		a = a * -1;
+		return (a);
+	}
+	else
+	{
+		return (a);
+	}
+}
