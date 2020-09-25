0121b29921610712fc2923600b713512ebb751c7
Xvaria <2349@holbertonschool.com>
2020-09-25 18:12:38 +0000

    Ready

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_square - draw a square
+ * Description: draw a square in the terminal
+ * @size: integer
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
