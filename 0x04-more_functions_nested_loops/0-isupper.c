5fb9bd4a6e259d139dd38af9693f48975c67e377
Xvaria <2349@holbertonschool.com>
2020-09-25 00:51:40 +0000

    Ready

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _isupper - Return 1 or 0 check character
+ * Description: checks for uppercase character
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if(c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,36 @@
+#ifndef holbertonHeader
+#define holbertonHeader
+
+int _putchar(char);
+
+/* 0-isupper.c */
+int _isupper(int);
+
+/* 1-isdigit.c */
+int _isdigit(int);
+
+/* 2-mul.c */
+int mul(int, int);
+
+/* 3-print_numbers.c */
+void print_numbers(void);
+
+/* 4-print_most_numbers.c */
+void print_most_numbers(void);
+
+/* 5-more_numbers.c */
+void more_numbers(void);
+
+/* 6-print_line.c */
+void print_line(int);
+
+/* 7-print_diagonal.c */
+void print_diagonal(int);
+
+/* 8-print_square.c */
+void print_square(int);
+
+/* 10-print-triangle.c */
+void print_triangle(int);
+
+#endif
