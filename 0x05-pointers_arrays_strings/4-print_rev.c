015dda67e998db50f5f41652cd56cb872d417f26
Xvaria <2349@holbertonschool.com>
2020-09-30 04:40:57 +0000

    Ready

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,22 @@
 #include "holberton.h"
 
+
+/**
+ * _strlen - length string
+ * Description: returns the length of a string
+ * @s: pointer
+ */
+int _strlen(char *s)
+{
+	char *a = s;
+
+	while (*s)
+	{
+		s++;
+	}
+	return (s - a);
+}
+
 /**
  * print_rev - print reverses a string
  * Description: prints a string, in reverse, followed by a new line
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,22 @@
 #include "holberton.h"
 
 /**
+ * _strlen - length string
+ * Description: returns the length of a string
+ * @s: pointer
+ */
+int _strlen(char *s)
+{
+	char *a = s;
+
+	while (*s)
+	{
+		s++;
+	}
+	return (s - a);
+}
+
+/**
  * rev_string - reverse
  * Description: reverses a string
  * @s: pointer
