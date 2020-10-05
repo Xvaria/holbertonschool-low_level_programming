2b7d4d6f930f0c4b7002e8e6e59f4f112ca76d80
Xvaria <2349@holbertonschool.com>
2020-10-05 23:15:32 +0000

    Ready

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _memset - fills memory
+ * Description: fills memory with a constant byte
+ * @s: array
+ * @b: constant
+ * @n: repeat
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n--)
+	{
+		*s = b;
+		s++;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,7 @@
+#ifndef holbertonHeader
+#define holbertonHeader
+
+/* 0-memset.c */
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
