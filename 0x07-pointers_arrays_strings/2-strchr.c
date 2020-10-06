752a2cd394ac02fc591712628d0e9722e8b50e4e
Xvaria <2349@holbertonschool.com>
2020-10-06 01:28:13 +0000

    Ready

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates
+ * Description: locates a character in a string
+ * @s: String
+ * @c: Character
+ * Return: s
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
