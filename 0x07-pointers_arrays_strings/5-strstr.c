7002c0502ad0e1ad1f85c1d05357c20365b1cbf9
Xvaria <2349@holbertonschool.com>
2020-10-06 16:07:14 +0000

    Correction v2.0

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,27 +12,28 @@ char *_strstr(char *haystack, char *needle)
 	int a = 0;
 	int b = 0;
 
-	while (haystack[a] != '\0')
+	while (haystack[a])
 	{
-		while (needle[b] != '\0')
+		while (needle[b] && (haystack[a] == needle[0]))
 		{
-			if (haystack[a] == needle[b])
+			if (haystack[a + b] == needle[b])
 			{
-				a++;
 				b++;
 			}
 			else
 			{
-				haystack++;
+				break;
 			}
 		}
+		if (needle[b])
+		{
+			a++;
+			b = 0;
+		}
+		else
+		{
+			return (haystack + a);
+		}
 	}
-	if (a == b)
-	{
-		return (haystack);
-	}
-	else
-	{
-		return ('\0');
-	}
+	return (0);
 }
