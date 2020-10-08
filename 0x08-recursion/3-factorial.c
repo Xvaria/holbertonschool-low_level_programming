402dc21c0c4c7e5abbd100674d9ed033a057062f
Xvaria <2349@holbertonschool.com>
2020-10-08 00:24:52 +0000

    Ready

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * factorial -  factorial
+ * Description: returns the factorial of a given number
+ * @n: number
+ * Return: -1 or factorial
+ */
+int factorial(int n)
+{
+	if(i < 0) {
+		return (-1);
+	}
+	return n * factorial(n - 1);
+}
