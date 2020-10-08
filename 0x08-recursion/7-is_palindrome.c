7a5461913332efd0d676d0c0d542ae8375ed9954
Xvaria <2349@holbertonschool.com>
2020-10-08 19:12:30 +0000

    Ready

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,53 @@
+#include "holberton.h"
+
+/**
+ * _strsize - Count string
+ * Description: count all letters of a string
+ * @s: String
+ * Return: size of a string to function is_palindrome
+ */
+int _strsize(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strsize(++s));
+}
+
+/**
+ * HPalindrome - Comparate in order an reverse
+ * Description: comparate string in order and reverse for return 1 or 0
+ * @str: String
+ * @i: initial position
+ * @f: size string
+ * Return: 1 to palindrome 0 if not
+ */
+int HPalindrome(char str[], int i, int f)
+{
+	if (i == f)
+	{
+		return (1);
+	}
+	if (str[i] != str[f])
+	{
+		return (0);
+	}
+	if (i < f + 1)
+	{
+		return (HPalindrome(str, i + 1, f - 1));
+	}
+	return (1);
+}
+
+/**
+ * is_palindrome - save count and string
+ * Description: returns to HPalindrome
+ * @s: String
+ * Return: function HPalindrome
+ */
+int is_palindrome(char *s)
+{
+	int size = _strsize(s) - 1;
+	return (HPalindrome(s, 0, size));
+}
