deae74b5c17a81ec3cc1bbeaf59cc7686c31be02
Xvaria <2349@holbertonschool.com>
2020-10-11 16:07:14 +0000

    Ready

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * printname - prints its name
+ * Description: prints its name, followed by a new line
+ * @argc: number
+ * @argv: name
+ * Return: 0
+ */
+int main(int argc, char* argv[])
+{
+	if (argc)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
