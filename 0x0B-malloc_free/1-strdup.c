dd5e70cc50319b8e27be38bb569ec1306d2d35b7
Xvaria <2349@holbertonschool.com>
2020-10-14 02:07:22 +0000

    Ready copy with malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * _strdup - newly allocated space in memory
+ * Description: returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
+ * @str: string
+ * Return: NULL or string
+ */
+char *_strdup(char *str)
+{
+	char *a;
+	int b = 0, c = 0;
+
+	while (str[b] != '\0')
+	{
+		b++;
+	}
+	a = malloc(sizeof(char) * b + 1);
+	if (*str)
+	{
+		while (c < b + 1)
+		{
+			a[c] = str[c];
+			c++;
+		}
+		return (a);
+	}
+	else
+	{
+		return ('\0');
+	}
+}
