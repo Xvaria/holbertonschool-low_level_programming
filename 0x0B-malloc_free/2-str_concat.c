73454621e92621137a8d24b4de426cf46c7b3341
Xvaria <2349@holbertonschool.com>
2020-10-14 04:37:03 +0000

    Ready Concatenate with malloc

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * str_concat - concatenate
+ * Description: concatenates two strings
+ * @s1: Betty
+ * @s2: Holberton
+ * Return: a or null
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *a;
+	int b = 0, c = 0, d = 0, e = 0;
+
+	while (s1[b])
+	{
+		b++;
+	}
+	while (s2[c])
+	{
+		c++;
+	}
+	d = b + c;
+	a = malloc (sizeof(char) * d + 1);
+	while (e < b)
+	{
+		a[e] = s1[e];
+		e++;
+	}
+	b = 0;
+	while (e <= d + 1)
+	{
+		a[e] = s2[b];
+		e++;
+		b++;
+	}
+	if (!a)
+	{
+		return ('\0');
+	}
+	return (a);
+}
