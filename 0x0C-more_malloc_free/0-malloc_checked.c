dd19b01652816a4bf49d18be66cef2f558f95651
Xvaria <2349@holbertonschool.com>
2020-10-15 22:48:50 +0000

    Ready allocates memory with malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * malloc_checked - allocates memory
+ * Description: allocates memory using malloc
+ * @b: size
+ * Return: a or termination status 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *a;
+
+	a = malloc(b);
+	if (!a)
+	{
+		exit(98);
+	}
+	return (a)
+}
---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,19 @@
+#ifndef holbertonHeader
+#define holbertonHeader
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 0-malloc_checked.c */
+void *malloc_checked(unsigned int b);
+
+/* 1-string_nconcat.c */
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+/* 2-calloc.c */
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+/* 3-array_range.c */
+int *array_range(int min, int max);
+
+#endif
