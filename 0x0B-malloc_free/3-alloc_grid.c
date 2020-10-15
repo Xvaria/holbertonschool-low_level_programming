4b6cfbec76f82d3bddfd293586044643c66fee54
Xvaria <2349@holbertonschool.com>
2020-10-15 06:33:19 +0000

    Correction v2.0 2D array with malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,36 +9,33 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **alloc_grid(int width, int height)
-	{
-	        int **a;
-	        int b = 0, c = 0;
+	int **a;
+	int b = 0, c = 0;
 
-	        if (width <= 0 || height <= 0)
-		{
-			return ('\0');
-		}
-	        a = malloc(sizeof(int *) * height);
-	        while (b < height)
-		{
-			a[b] = malloc(sizeof(int) * width);
-			b++;
-		}
-	        b = 0;
-	        if (a != '\0')
-		{
-			while (b < height)
-			{
-				while (c < width)
-				{
-					a[b][c] = 0;
-					c++;
-				}
-				b++;
-				c = 0;
-			}
-			return (a);
-		}
-	        return ('\0');
+	if (width <= 0 || height <= 0)
+	{
+		return ('\0');
 	}
+	a = malloc(sizeof(int *) * height);
+	while (b < height)
+	{
+		a[b] = malloc(sizeof(int) * width);
+		b++;
+	}
+	b = 0;
+	if (a != '\0')
+	{
+		while (b < height)
+		{
+			while (c < width)
+			{
+				a[b][c] = 0;
+				c++;
+			}
+			b++;
+			c = 0;
+		}
+		return (a);
+	}
+	return ('\0');
 }
