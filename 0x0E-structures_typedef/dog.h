481ab257c2f69ee0ba39c8f283ca6555b640d806
Xvaria <2349@holbertonschool.com>
2020-10-19 19:41:34 +0000

    Ready structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - Define a new type struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner's name
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
