ce43e7961ef4dc5f2e489c37d3c1dd9e2ad94f3d
Xvaria <2349@holbertonschool.com>
2020-10-21 20:03:05 +0000

    Ready Pointers to functions

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: Bob, Bob Dylan
+ * @f: pointer to functions
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!f)
+	{
+		f(name);
+	}
+}
