bbc96b60b5b98f05dcb9c3944ab30ca0c87f1a76
Xvaria <2349@holbertonschool.com>
2020-10-21 21:08:43 +0000

    Correction v2.0 fix array and while

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,14 +9,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int a = 0;
-
 	if (array && action)
 	{
-		while(a < size)
+		while(size--)
 		{
-			action(array[a]);
-			a++;
+			action(*(array++));
 		}
 	}
 }
