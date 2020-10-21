55a167283384fad78d488f836b59a6b8d0cb78dd
Xvaria <2349@holbertonschool.com>
2020-10-21 21:02:28 +0000

    Correction v2.0 add comits and description, and add iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,17 +1,22 @@
 #include "function_pointers.h"
 
 /**
- * 
- * 
- * 
+ * array_iterator - executes a function given as a parameter on each element of
+ * an array
+ * @array: array of numbers
+ * @size: number of numbers to print
+ * @action: functions
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	int a = 0;
+
 	if (action && action)
 	{
-		while(size--)
+		while(a < size)
 		{
-			action(array++);
+			action(array[a]);
+			a++;
 		}
 	}
 }
