fdb02d54dada8cd12ab2d893e897bf08e3a63ee5
Xvaria <2349@holbertonschool.com>
2020-10-22 04:39:36 +0000

    Ready performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+
+/**
+ * main - performs simple operations
+ * @argc: number of parameters
+ * @argv: parameters
+ * Return: 0 or r
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, c;
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((a == 0 || b == 0) && (*argv[2] == '/' || *argv[2] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	c = get_op_func(argv[2])(a, b);
+	printf("%i\n", c);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add
+ * @a: first digit
+ * @b: second digit
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub
+ * @a: first digit
+ * @b: second digit
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - mul
+ * @a: first digit
+ * @b: second digit
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - div
+ * @a: first digit
+ * @b: second digit
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - mod
+ * @a: first digit
+ * @b: second digit
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
