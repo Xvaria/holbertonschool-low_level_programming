e9b37e57b51a00ffe6af32b548d05b57b36caa4a
Xvaria <2349@holbertonschool.com>
2020-10-24 17:54:03 +0000

    Correction v2.0 fix if to separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,16 +10,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list valist;
 	unsigned int a = 0;
 
-	if (separator)
+	va_start(valist, n);
+	while (a < n - 1)
 	{
-		va_start(valist, n);
-		while (a < n - 1)
-		{
-			printf("%i%s", va_arg(valist, int), separator);
-			a++;
-		}
 		printf("%i", va_arg(valist, int));
-		va_end(valist);
+		if (separator)
+		{
+			printf("%s", separator)
+		}
 	}
-	printf("\n");
+	printf("%i", va_arg(valist, int));
+	va_end(valist);
 }
