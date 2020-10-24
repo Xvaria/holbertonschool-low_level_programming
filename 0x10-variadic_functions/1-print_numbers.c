02fcf00f6c92a8f35165654f3c6cb1974fe36285
Xvaria <2349@holbertonschool.com>
2020-10-24 18:12:21 +0000

    Correction v2.02 declarate int p and asign arguments

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,17 +9,20 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
 	unsigned int a = 0;
+	int p;
 
 	va_start(valist, n);
 	while (a < n - 1)
 	{
-		printf("%i", va_arg(valist, int));
+		p = va_arg(valist, int);
+		printf("%i", p);
 		if (separator)
 		{
 			printf("%s", separator);
 		}
 		a++;
 	}
-	printf("%i\n", va_arg(valist, int));
+	p = va_arg(valist, int);
+	printf("%i\n", p);
 	va_end(valist);
 }
