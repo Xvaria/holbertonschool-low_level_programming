3cd66560bab6251156ac5a26252c055a95ad3467
Xvaria <2349@holbertonschool.com>
2020-11-05 04:55:55 +0000

    Ready Get node at index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list
+ * @head: list
+ * @index: value
+ * Return: nn
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *nn;
+	int a = 0;
+
+	nn = head;
+	while (nn)
+	{
+		if (nn == index)
+		{
+			return (nn);
+		}
+		a++;
+		nn = nn->next;
+	}
+	return (nn);
+}
