8f64d355d2d012b56ddaaa5692115521454ce23f
Xvaria <2349@holbertonschool.com>
2020-11-06 06:06:03 +0000

    Correction v2.02 clean and modify assignment to n

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,21 +8,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int m = 1;
-
-	if (index > sizeof(n) * 8 - 1)
+       	if (index > sizeof(n) * 8 - 1)
 	{
 		return (-1);
 	}
-	if (*n == 0)
-	{
-		*n = 0;
-		return (1);
-	}
-	while (index--)
-	{
-		m *= 2;
-	}
-	*n -= m;
+	*n &= ~(1UL << index);
 	return (1);
 }
