df28a695e64d543e184a6118654b4db9a55b6038
Xvaria <2349@holbertonschool.com>
2020-11-10 05:00:02 +0000

    Correction v2.0 add if for open file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	a = open(filename, O_RDWR | O_CREAT | O_TRUNC, 600);
+	a = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (a == -1)
 		return (-1);
 	if (text_content)
