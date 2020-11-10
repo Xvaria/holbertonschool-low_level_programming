b95f3cd8468e88c2c477fe7ce094d6074d518b80
Xvaria <2349@holbertonschool.com>
2020-11-10 04:55:40 +0000

    Ready creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * create_file - create a file
+ * @filename: name to the file
+ * @text_content: string to file
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int a, e, l = 0;
+
+	if (!filename)
+		return (-1);
+	a = open(filename, O_RDWR | O_CREAT | O_TRUNC, 600);
+	if (text_content)
+	{
+		while (text_content[l])
+			l++;
+		e = write(a, text_content, l);
+		if (e == -1)
+		{
+			return (-1);
+		}
+	}
+	close(a);
+	return (1);
+}
