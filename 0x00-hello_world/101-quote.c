bbf11d2344ea9b9a1e5a1bdf39633682e98404ee
Xvaria <2349@holbertonschool.com>
2020-11-27 22:12:58 +0000

    Ready Write a C program that prints exactly text

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * main - Write a C program that prints exactly text
+ * Return: 1
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19", 57);
+	return (1);
+}
