83bda39fd6a33cdf0913402ac980c5d05dd775d6
Xvaria <2349@holbertonschool.com>
2020-12-14 18:12:09 +0000

    Ready adds a new node at the beginning of a dlistint_t list

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list
+ * @head: structure
+ * @n: element
+ * Return: head or NULL
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *nn;
+
+	if (n)
+	{
+		nn = malloc(sizeof(dlistint_t));
+		if (nn)
+		{
+			nn->n = n;
+			nn->next = *head;
+			*head = nn;
+			return (*head);
+		}
+	}
+	return (NULL);
+}
