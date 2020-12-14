008523631bdee2098f4dc3c1566ffa8e9b5ebc27
Xvaria <2349@holbertonschool.com>
2020-12-14 06:52:39 +0000

    Ready prints all the elements of a dlistint_t list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: strcuture
+ * Return: a
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t a = 0;
+	const dlistint_t *ht = h;
+	while(ht)
+	{
+		printf("%i\n", ht->n);
+		ht = ht->next;
+		a++;
+	}
+	return (a);
+}
