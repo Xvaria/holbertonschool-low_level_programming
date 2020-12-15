4074b2ec70b23b043ff84fa736539271acc20664
Xvaria <2349@holbertonschool.com>
2020-12-15 02:33:09 +0000

    Ready deletes the node at index index of a dlistint_t linked list

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes the node at index index of a
+ * dlistint_t linked list
+ * @head: structure
+ * @index: index
+ * Return:  1 or -1
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *nt;
+
+	if (!head || !*head)
+		return (-1);
+
+	nt = *head;
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(nt);
+		return (1);
+	}
+	while (nt->next != NULL)
+	{
+		if (i == index)
+		{
+			nt->next->prev = nt->prev;
+			nt->prev->next = nt->next;
+			free(nt);
+			return (1);
+		}
+		nt = nt->next;
+		i++;
+	}
+
+	return (-1);
+}
