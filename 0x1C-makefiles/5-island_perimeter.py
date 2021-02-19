de6c0f89d4e4f0dd5335bb60fcc0eed0a68de4f8
Xvaria <2349@holbertonschool.com>
2021-02-19 22:51:03 +0000

    Correction v2.03 update conditions

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,21 +7,18 @@ def island_perimeter(grid):
     i = 0
     j = 0
     p = 0
-    if len(grid) > 100 or len(grid[j]) > 100:
-        pass
-    else:
-        while i < len(grid):
-            while j < len(grid[i]):
-                if grid[i][j] == 1:
-                    if grid[i-1][j] == 0:
-                        p += 1
-                    if grid[i+1][j] == 0:
-                        p += 1
-                    if grid[i][j+1] == 0:
-                        p += 1
-                    if grid[i][j-1] == 0:
-                        p += 1
-                j += 1
-            j = 0
-            i += 1
+    while i < len(grid):
+        while j < len(grid[i]):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i-1][j] == 0:
+                    p += 1
+                if i == (len(grid) - 1) or grid[i+1][j] == 0:
+                    p += 1
+                if j == (len(grid) - 1) or grid[i][j+1] == 0:
+                    p += 1
+                if j == 0 or grid[i][j-1] == 0:
+                    p += 1
+            j += 1
+        j = 0
+        i += 1
     return p
