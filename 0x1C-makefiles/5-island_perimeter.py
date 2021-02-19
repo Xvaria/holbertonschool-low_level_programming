dafd07c794ef22a30f91f2484f7f09145f8ae314
Xvaria <2349@holbertonschool.com>
2021-02-19 20:30:14 +0000

    Ready Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'Island Perimeter'
+
+
+def island_perimeter(grid):
+    'returns the perimeter of the island described in grid'
+    i = 0
+    j = 0
+    p = 0
+    while i < len(grid):
+        while j < len(grid[i]):
+            if grid[i][j] == 1:
+                if grid[i-1][j] == 0:
+                    p += 1
+                if grid[i+1][j] == 0:
+                    p += 1
+                if grid[i][j+1] == 0:
+                    p += 1
+                if grid[i][j-1] == 0:
+                    p += 1
+            j += 1
+        j = 0
+        i += 1
+    return p
