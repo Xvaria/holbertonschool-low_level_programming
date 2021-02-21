03a008d605877c506c3f6a57fc1ff3e9b765f2bf
Xvaria <2349@holbertonschool.com>
2021-02-21 18:52:12 +0000

    Correction v2.0 add multiples of 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,16 +6,24 @@
  */
 int main(void)
 {
-	long t = 0, n = 1, s = 0;
+	long t = 0, c = 0, nt = 1, nc = 1, s = 0;
 
-	while (t < 1024)
+	while (t < 1024 && c < 1024)
 	{
-		t = 3 * n;
-		if (t < 1024)
+		t = 3 * nt;
+		c = 5 * nc;
+		if (c < t)
 		{
-			s += t;
+			if (c < 1024)
+				s += c;
+			nc++;
 		}
-		n++;
+		if (t != c)
+		{
+			if (t < 1024)
+				s += t;
+		}
+		nt++;
 	}
 	printf("%ld\n", s);
 	return (0);
