31027fdc8e7baf41c0175473c1dddb797baa6d3d
Xvaria <2349@holbertonschool.com>
2021-04-20 21:16:39 +0000

    Ready | Binary search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,39 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array of integers using the
+ * Linear search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: first index where value is located or -1 if array is NULL or no match
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, poslow = 0, postop = size - 1, pos = 0;
+
+	if (array)
+		while (poslow <= postop)
+		{
+			pos = (poslow + postop) / 2;
+
+			printf("Searching in array: ");
+			for (i = poslow; i <= postop; i++)
+			{
+				if (i != postop)
+					printf("%d, ", array[i]);
+				else
+					printf("%d", array[i]);
+			}
+			printf("\n");
+
+			if (array[pos] < value)
+				poslow = pos + 1;
+			else if (array[pos] > value)
+				postop = pos - 1;
+			else
+				return (pos);
+		}
+	return (-1);
+}
